b9da0b27b07d0db00c68e8ce15d8e16913635011
victorpazs <victorps.2004@gmail.com>
2023-02-08 23:19:04 -0300

    filter e post

--- src/jobs/index.js
+++ src/jobs/index.js
@@ -3,7 +3,17 @@ import * as Jobs from "./logic.js";
 const jobsRouter = Router();
 
 jobsRouter.get("/", (req, res) => {
-  Jobs.getJobs(req, (data, error) => {
+  Jobs.getJobs(req.query, (data, error) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+jobsRouter.post("/", (req, res) => {
+  Jobs.createJob(req.body, (data, error) => {
     if (error) {
       res.status(500).send(error);
     } else {
--- src/jobs/logic.js
+++ src/jobs/logic.js
@@ -1,10 +1,40 @@
 import client from "../../database.js";
 
 export function getJobs(data, callback = console.log) {
-  client.query("SELECT * FROM jobs", (error, res) => {
+  let where = "";
+
+  if (data.search) {
+    where += `and title ilike '%${data.search}%'`;
+  }
+
+  const sql = `SELECT * FROM jobs where status in(1,2) ${where}`;
+
+  client.query(sql, (error, res) => {
     if (error) {
       return callback(undefined, error);
     }
     callback(res.rows);
   });
+}
+
+export function createJob(data, callback = console.log) {
+  let values = [
+    data.title,
+    data.description,
+    data.status || 1,
+    data.salary,
+    data.period,
+    data.img,
+  ];
+
+  client.query(
+    "INSERT INTO jobs(title, description, status, salary, period, img) VALUES($1,$2,$3,$4,$5,$6)",
+    values,
+    (error, res) => {
+      if (error) {
+        return callback(undefined, error);
+      }
+      callback(res.rows);
+    }
+  );
 }
